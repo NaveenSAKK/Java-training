52b98b3c92bffdd7c0eba00ebc13680d8d08c071
NaveenSAKK <55385372+NaveenSAKK@users.noreply.github.com>
2023-05-11 17:51:08 +0530

    Add files via upload

---
+++ first_quarkas/amms/src/main/java/com/del/MenuRepo.java
@@ -0,0 +1,9 @@
+package com.del;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class MenuRepo implements PanacheRepository<Menu> {
+
+}
